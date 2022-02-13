dbe60565d2a4bccc05990d0974137bbc36080249
FranckNk <nkeuboufranck@gmail.com>
2022-02-13 12:14:01 -0400

    Changement du nom de dossier du uC ESP32 en MKR1000  et ajout d'un nouveau projet pour le uC MKR1010. Ajout du code pour envoi et reception de l'information suivant le protocole I2C entre les deux uC.

---
+++ ConnexionBetween2uC_MKR1000/src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "Timer.h"
+
+Timer Temp;
+// Include the required Wire library for I2C<br>#include 
+const int PIN_ROTATION_SENSOR = A0;
+
+unsigned short int ValueAngleServo = 0;        // Valeur envoyee dans le servo moteur.
+unsigned short int RotationSensorValue = 0;        // Valeur lue par le capteur de roation.
+unsigned short int LastValueAngleServo = 90; 		// Derniere valeur de l'angle du servo moteur.
+
+
+unsigned long TimeDelay = 2000;
+
+void setup() {
+	// Start the I2C Bus as Master
+	Serial.begin(9600);
+	pinMode(PIN_ROTATION_SENSOR, INPUT);
+	Wire.begin(); 
+	Temp.startTimer(3000);
+}
+void loop() {
+	RotationSensorValue = analogRead(PIN_ROTATION_SENSOR);
+	ValueAngleServo = map(RotationSensorValue, 0, 1023, 0, 180);
+	if (ValueAngleServo != LastValueAngleServo){
+		// On a obtenu une variation de 02 degres soit de plus ou soit de moins.
+		if (Temp.isTimerReady())
+		{
+			Serial.print("\n\nVariation OBTENUE.\n\n = ");
+			Serial.println(ValueAngleServo);
+			Temp.startTimer(TimeDelay);		
+		}
+		// On envoie le signal via le protocole I2C.
+		Wire.beginTransmission(9); // transmit to device #9
+		Wire.write(ValueAngleServo);              // sends x 
+		Wire.endTransmission();    // stop transmitting
+		LastValueAngleServo = ValueAngleServo; // On sauvegarde la derniere variation.
+		} 
+		// // print the results to the serial monitor:
+	Serial.print("sensor = ");
+	Serial.print(RotationSensorValue);
+	Serial.print("\t output = ");
+	Serial.println(ValueAngleServo);
+	delay(200);
+}
---
+++ ConnexionBetween2uC_MKR1010/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Servo.h>
+#include "Timer.h"
+// Declaration des classes.
+Servo ServoMoteur;  // Création d'un objet de type Servo (un servo moteur).
+Timer Temp;
+
+unsigned long TimeDelay = 2000;
+
+unsigned short int ValueGot = 0;
+void receiveEvent(int bytes);
+void setup() {
+	// Start the I2C Bus as Slave on address 9
+	Wire.begin(9); 
+	Serial.begin(9600);
+	// Attach a function to trigger when something is received.
+	Wire.onReceive(receiveEvent);
+	Temp.startTimer(3000);
+}
+void receiveEvent(int bytes) {
+	ValueGot = Wire.read();    // read one character from the I2C
+}
+void loop() {
+	if (Temp.isTimerReady())
+	{
+		Serial.print("\nLa valeur obtenue est : ");
+		Serial.println(ValueGot);
+		Temp.startTimer(TimeDelay);		
+	}
+	
+	
+}
